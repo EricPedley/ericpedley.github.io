1fd93e87bd765649ccf443f1214513f3caa7da5f
Eric Pedley <ericpedley@gmail.com>
2025-10-15 14:53:08 -0700

    update ksacle project and temporarily comment everything else out

--- extra_projects/kscale.md
+++ extra_projects/kscale.md
@@ -1,6 +1,4 @@
 # K-Scale Labs Internship
-![project screenshot](https://github.com/EricPedley/ericpedley.github.io/blob/main/images/kscale.png?raw=true)
+![project screenshot](/images/kscale_compilation.gif)
 ## Description
-This summer I had the great opportunity to work at K-Scale Labs for a month. The opportunity really came out of nowhere. I was living with my parents and working on personal projects during the summer between graduating from UC Irvine and starting my masters degree at UCSD, and joined a few robotics discord servers to get project inspiration and chat with others in the open-source robotics community. One day I was checking the server and saw Ben (the CEO) posting about issues they were having with getting good calibration for the fisheye cameras on their robot. I had worked on this exact problem for my A2RL team before, and had been working on a library to make calibration easier as one of my summer projects, so I replied with some tips and an offer to come over to the K-Scale house and help them out. At the time I was just thinking it would be cool to visit and see them working on the robot while also getting a chance to work out the kinks in my calibration library. 
-
-TODO: write more
+This summer I interned at K-Scale Labs for a month. I worked on a teleoperation system for their humanoid robot that allowed control of the robot from a VR headset. I touched all parts of the robotics stack, including modifying firmware, training RL policies, writing custom inverse kinematics, and working on camera calibration and rendering.
--- js/addCards.js
+++ js/addCards.js
@@ -1,21 +1,21 @@
 async function addCards() {
     const user = "EricPedley"
     const repoList = [
+        "/extra_projects/kscale.md",
         "/extra_projects/a2rl.md",
         "/extra_projects/uav_forge.md",
-        "/extra_projects/kscale.md",
-        "https://github.com/UCI-Rocket-Project/rocket-tracker/",
-        "https://github.com/EricPedley/roomba_ws",
-        "https://github.com/EricPedley/erics_cameras",
-        "https://github.com/EricPedley/so101",
-        "https://github.com/Cyclone-Labs/Simple-Quad-RL",
-        "https://github.com/EricPedley/cs179-final",
-        "/extra_projects/cs112.md",
-        "https://github.com/EricPedley/M5StampFly",
-        "/extra_projects/antalmanac.md",
-        "/extra_projects/master_iol.md",
-        "/extra_projects/ship_detection.md",
-        "/extra_projects/zotmeal.md",
+        // "https://github.com/EricPedley/so101",
+        // "https://github.com/UCI-Rocket-Project/rocket-tracker/",
+        // "https://github.com/EricPedley/roomba_ws",
+        // "https://github.com/EricPedley/erics_cameras",
+        // "https://github.com/Cyclone-Labs/Simple-Quad-RL",
+        // "https://github.com/EricPedley/cs179-final",
+        // "/extra_projects/cs112.md",
+        // "https://github.com/EricPedley/M5StampFly",
+        // "/extra_projects/antalmanac.md",
+        // "/extra_projects/master_iol.md",
+        // "/extra_projects/ship_detection.md",
+        // "/extra_projects/zotmeal.md",
     ]
     const container = document.querySelector(".card-container");
     for(const repo of repoList) {
